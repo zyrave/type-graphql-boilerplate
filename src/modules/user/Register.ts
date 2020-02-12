a5e5d72350e76e246c6ac24bc068d3f115212429
zyrave <rob.rivermark@gmail.com>
2020-02-12 14:11:39 +0700

    Fix authentication

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -3,22 +3,17 @@ import bcrypt from 'bcryptjs';
 
 import { User } from '../../entities/User';
 import { RegisterInput } from './types/RegisterInput';
-import { isAuthenticated } from '../middleware/isAuthenticated';
 import { sendEmail } from './../utils/sendEmail';
 import { createConfirmationUrl } from '../utils/createConfirmationUrl';
 
 @Resolver()
 export class RegisterResolver {
   @Query(() => String)
-  @UseMiddleware(isAuthenticated)
   @Mutation(() => User)
-  async register(@Arg('data')
-  {
-    firstName,
-    lastName,
-    email,
-    password,
-  }: RegisterInput): Promise<User> {
+  async register(
+    @Arg('data')
+    { firstName, lastName, email, password }: RegisterInput,
+  ): Promise<User> {
     const hashedPassword = await bcrypt.hash(password, 12);
 
     const user = await User.create({
